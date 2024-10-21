27b1e5e924f5c9aeada2c4533bf7a206132cebeb
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-21 20:04:21 -0300

    Ejemplo de lazy loading

--- 6-consultation-involing-associations.js
+++ 6-consultation-involing-associations.js
@@ -18,7 +18,18 @@ const Captain = sequelize.define('Captain', {
 Captain.hasOne(Ship)
 Ship.belongsTo(Captain)
 
+await sequelize.sync({ force: true })
+
 // Example of lazy loading
 const awesomeCaptain = await Captain.findOne({
-
+  where: {
+    name: 'Jack Sparrow'
+  }
 })
+console.log('Name: ', awesomeCaptain?.name)
+console.log('Skill Level: ', awesomeCaptain?.skillLevel)
+
+// Ahora necesitamos la información de su barco
+const hisShip = await awesomeCaptain?.getShip()
+console.log('Ship Name: ', hisShip?.name)
+console.log('Amount of Sails: ', hisShip?.amountOfSails)
