0f2b795967185fc289c31fb8e3f8d7ba048b8578
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-21 20:21:37 -0300

    Ejemplo de eager loading

--- 6-consultation-involing-associations.js
+++ 6-consultation-involing-associations.js
@@ -20,7 +20,7 @@ Ship.belongsTo(Captain)
 
 await sequelize.sync({ force: true })
 
-// Example of lazy loading
+// ------ ------ Example of lazy loading ------ ------
 const awesomeCaptain = await Captain.findOne({
   where: {
     name: 'Jack Sparrow'
@@ -33,3 +33,16 @@ console.log('Skill Level: ', awesomeCaptain?.skillLevel)
 const hisShip = await awesomeCaptain?.getShip()
 console.log('Ship Name: ', hisShip?.name)
 console.log('Amount of Sails: ', hisShip?.amountOfSails)
+
+// ------ ------ Example for eager loading ------ ------
+const anotherAwesomeCaptain = await Captain.findOne({
+  where: {
+    name: 'Jack Sparrow'
+  },
+  include: Ship
+})
+// Ahora la información del barco viene con el capitan
+console.log('Name: ', anotherAwesomeCaptain?.name)
+console.log('Skill Level: ', anotherAwesomeCaptain?.skillLevel)
+console.log('Ship Name: ', anotherAwesomeCaptain?.ship?.name)
+console.log('Amount of Sails: ', anotherAwesomeCaptain?.ship?.amountOfSails)
